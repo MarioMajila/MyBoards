e1c1042abee8a4501cf3b0d14060f0d3c3eedfad
MarioMajila <mariustshipichik@gmail.com>
2019-03-12 21:46:22 +0100

    Initialisation des modules

---
+++ app.js
@@ -0,0 +1,6 @@
+// Initialisation des modules 
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserView
+const path = require('path')
+const url = require('url')
