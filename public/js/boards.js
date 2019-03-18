bef44a051853b7a14b15231c18d901e19b592aaa
MarioMajila <mariustshipichik@gmail.com>
2019-03-18 10:48:11 +0100

    Definition de la fonction d'écoute des événements

--- public/js/boards.js
+++ public/js/boards.js
@@ -2,3 +2,9 @@
 const projectName = document.querySelector('#name')
 const projectType = document.querySelector('#type')
 const description = document.querySelector('#description')
+
+LoadEventListener();
+
+function LoadEventListener(){
+
+}
