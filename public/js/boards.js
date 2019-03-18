72de316c007ec5a67e705cab98577488fc693950
MarioMajila <mariustshipichik@gmail.com>
2019-03-18 10:51:20 +0100

    ADD Initialisation de l'événement au click sur le bouton d'enregistrement

--- public/js/boards.js
+++ public/js/boards.js
@@ -2,9 +2,11 @@
 const projectName = document.querySelector('#name')
 const projectType = document.querySelector('#type')
 const description = document.querySelector('#description')
+const btnEnregistrer = document.querySelector('#btnEnregistrer')
 
 LoadEventListener();
 
 function LoadEventListener(){
 
+    btnEnregistrer.addEventListener('click', addProject);
 }
