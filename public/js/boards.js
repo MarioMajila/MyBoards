7e6207748368e6f997d8479366ad79436b293bec
MarioMajila <mariustshipichik@gmail.com>
2019-03-18 10:25:34 +0100

    ADD initialisation des variables

---
+++ public/js/boards.js
@@ -0,0 +1,4 @@
+// Initialisation des variables
+const projectName = document.querySelector('#name')
+const projectType = document.querySelector('#type')
+const description = document.querySelector('#description')
