15ea7c6a055aa56dbf3c4736e11a2b608c137f28
MarioMajila <mariustshipichik@gmail.com>
2019-03-20 21:08:06 +0100

    CHANGE nouveau départ sur la fonction addProject

--- public/js/boards.js
+++ public/js/boards.js
@@ -3,42 +3,25 @@ const $ = require('jquery');
 
 $(() => {
     // Initialisation des variables
-    const projectName = document.querySelector('#name')
-    const projectType = document.querySelector('#type')
-    const description = document.querySelector('#description')
-    const btnEnregistrer = document.querySelector('#btnEnregistrer')
+    const projectName = $('#name')
+    const projectType = $('#type')
+    const description = $('#description')
+    const btnEnregistrer = $('#btnEnregistrer')
 
-    const projectCollection = document.querySelector('#projectCollection')
+    const projectCollection = $('#projectCollection')
 
     LoadEventListener();
 
     function LoadEventListener(){
 
-        btnEnregistrer.addEventListener('click', addProject);
+        btnEnregistrer.on('click', addProject);
     }
 
     // Ajout d'un nouveau projet
     function addProject(event){
         if(projectName.value == ""){
             alert('Veuillez donner un nom à votre projet');
-        }
-
-        // Création d'un tableau
-        const div = document.createElement('div');
-        div.className = "card col-sm-3 project";
-
-        // Création du nom du project
-        const h2 = document.createElement('h2');
-        h2.className = "project-title";
-        h2.appendChild(document.createTextNode(projectName.value));
-        
-        div.appendChild(h2);
-        alert(div)
-        // alert('Projet ajouté !')
-        storeProjectInLocalStorage(projectName.value);
-
-        projectCollection.appendChild(h2);
-        
+        }        
 
         event.preventDefault();
     }
